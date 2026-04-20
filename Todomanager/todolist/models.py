439d893644f024042b47c945a914f78816da0b15
pravin5062804 <pravin.choudhary.5062804@ves.ac.in>
2026-04-21 03:01:02 +0530

    CRUD operations Learned ORM in django

--- Todomanager/todolist/models.py
+++ Todomanager/todolist/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Task(models.Model):
     task=models.CharField(max_length=100)
-    is_completed=models.BooleanField()
+    is_completed=models.BooleanField(default=False)
     #iske wajah se admin panel pe read kar sakte ho obejct by using magic method str
     def __str__(self):
         return self.task
--- Todomanager/todolist/views.py
+++ Todomanager/todolist/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from todolist.models import Task
+from todolist.forms import taskform
 
 
 # Create your views here.
@@ -12,6 +13,12 @@ def home(request):
     return render(request,"home.html",context)
 
 def todolist(request):
+    if request.method=='POST':
+        form_data=taskform(request.POST or None)
+        if form_data.is_valid():
+            form_data.save()
+            return redirect("todolist")
+                
     all_task=Task.objects.all()
     context={
         'page':"todolist  page",
